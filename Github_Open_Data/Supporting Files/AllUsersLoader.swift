a942b29aec850006e75ebd7c9062502f9d6f9f0f
Jerome Hsieh <jerome.developer.tw@gmail.com>
2020-05-30 17:12:24 +0800

    Implement pagination in AllUserLoader

--- Github_Open_Data/Supporting Files/AllUsersLoader.swift
+++ Github_Open_Data/Supporting Files/AllUsersLoader.swift
@@ -14,22 +14,55 @@ typealias UserElementViewModelsDownloadedCompletion = ([UserElementViewModel]) -
 class AllUsersLoader {
   
   // MARK:- state and initializer
-  private var since: Int = 0
-  private var perPage: Int = 20
+  private var firstSince: Int = 0
+  private var firstPerPage: Int = 20
   
-  private var queryURLString: String {
-    return "https://api.github.com/users?since=\(since)&per_page=\(perPage)"
+  private var fisrtQueryURLString: String {
+    return "https://api.github.com/users?since=\(firstSince)&per_page=\(firstPerPage)"
   }
   
-  init(since: Int = 0, perPage: Int = 20) {
-    self.since = since
-    self.perPage = perPage
+  private var nextQueryURLString: String?
+  private var isLoading = false
+  
+  init(firstSince: Int = 0, firstPerPage: Int = 20) {
+    self.firstSince = firstSince
+    self.firstPerPage = firstPerPage
   }
   
   // MARK:- implementation of business logic
   func load(completion: @escaping UserElementViewModelsDownloadedCompletion) {
-    AF.request(queryURLString).response { response in
-      if let data = response.data {
+    guard isLoading == false else {
+      print("Do not load until the last load is completed")
+      return
+    }
+    
+    var localURLString = ""
+    if let nextQueryURLString = nextQueryURLString {
+      localURLString = nextQueryURLString
+    } else {
+      localURLString = fisrtQueryURLString
+    }
+    isLoading = true
+    AF.request(localURLString).response { [weak self] response in
+      defer {
+        self?.isLoading = false
+      }
+      if let data = response.data, let headerFields = response.response?.allHeaderFields, let nextLinkString = headerFields["Link"] as? String {
+        // nextLinkString will be something like:
+        // <https://api.github.com/users?since=19&per_page=5>; rel="next", <https://api.github.com/users{?since}>; rel="first"
+        print("nextLinkString: \(nextLinkString)")
+        let findingKey = "; rel=\"next\""
+        var foundNextLink = ""
+        guard let tempString = nextLinkString.components(separatedBy: ",").filter({ string -> Bool in
+          string.contains(findingKey)
+        }).first else {
+          assertionFailure()
+          return
+        }
+        foundNextLink = tempString.replacingOccurrences(of: findingKey, with: "").replacingOccurrences(of: "<", with: "").replacingOccurrences(of: ">", with: "")
+        print("foundNextLink: \(foundNextLink)")
+        self?.nextQueryURLString = foundNextLink
+        
         let decoder = JSONDecoder()
         decoder.dateDecodingStrategy = .iso8601
         do {
